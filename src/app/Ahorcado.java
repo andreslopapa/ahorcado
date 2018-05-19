57f162ca731c66dd0d409206aafb584cf6fabd9b
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2018-05-19 19:10:55 -0300

    Converting to maven project

--- src/app/Ahorcado.java
+++ src/app/Ahorcado.java
@@ -1,6 +1,10 @@
 package app;
 
 public class Ahorcado {
+	
+	public static void main(String [] args) {
+		System.out.println("Hello world");
+	}
 
 	public String helloAhorcado() {
 		return "Hello";
